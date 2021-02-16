b1aae8cb78cc0b7de6b7d48f0af856d8fd20ff5c
Rganeshmoorthy <rganeshmoorthy1997@gmail.com>
2021-02-16 14:51:30 +0530

    ganesh_compfie

---
+++ domain-page4.php
@@ -0,0 +1,66 @@
+<?php  include "header.php"?>
+<?php include "nav-pills.php"?>
+<?php include "sidenav.php" ?>
+<html>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/domain-page4.css">
+<script>
+</script>
+
+<body>
+    <div class="main-content">
+        <!-- center content starts -->
+        <div class="row px-5 px-md-5 py-3 main-content-3">
+            <div class="col-12 p-0 heading">
+                Labour,Employment & Industrial - State Acts
+            </div>
+            <!-- <div class="col-12 py-3">
+                <div class="d-flex justify-content-around">
+                    <div class="border search-div px-2">
+                        <input type="text" placeholder="search" class="border-0">
+                        <button class="border-0 bg-transparent search-btn"><i class="fa fa-search"></i></button>
+                    </div>
+                    <div class="px-2 select-country">
+                        <select class="form-control" onfocus='this.size=5;' onblur='this.size=1;'
+                            onchange='this.size=1; this.blur();'>
+                            <option selected>Select State</option>
+                            <option>Andaman Nicobar</option>
+                            <option>Andrapradesh</option>
+                            <option>mathyapradesh</option>
+                            <option>kerala</option>
+                            <option>karnataka</option>
+                            <option>Assam</option>
+                            <option>Bihar</option>
+                            <option>Rajastan</option>
+                            <option>TamilNadu</option>
+                        </select>
+                    </div>
+                </div>
+            </div> -->
+            <div class="col-12 px-3 py-4">
+                <div class="row">
+                    <div class="col-12 heading bg-color py-1">
+                        All ACTS
+                    </div>
+                    <div class="col-12 content-bgcolor">
+                        <ol type="1" class="ol-content p-1 pl-3">
+                            <li class="mt-2">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                            <li class="mt-3">Assam Lifts And Escalators Act, 1990 </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
---
+++ nav-pills.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="package/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="css/nav-pill.css">
+</head>
+
+<body>
+    <div class="container nav_pill w-100 p-0">
+        <ul id="myTabs" class="nav nav-pills nav-justified nav_ul w-100" role="tablist" data-tabs="tabs">
+            <li class="active px-5 ml-0 ml-md-5"><a href="domain-page3.php" data-toggle="tab">Labour,Employment &
+                    Industrial</a>
+            </li>
+            <li class="px-5"><a href="domain-page.php" data-toggle="tab">Environmental,Health & Safety</a></li>
+            <li class="px-5 px-md-1"><a href="domain-page.php" data-toggle="tab">Fiscal,Corporate & Commercial</a>
+            </li>
+        </ul>
+        <!-- <div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active" id="Commentary">Commentary WP_Query goes here.</div>
+            <div role="tabpanel" class="tab-pane fade" id="Videos">Videos WP_Query goes here.</div>
+            <div role="tabpanel" class="tab-pane fade" id="Events">Events WP_Query goes here.</div>
+        </div> -->
+    </div>
+</body>
+
+</html>
