80665fed1db93e98c445de87f5e9dbed2a6e360f
Rganeshmoorthy <rganeshmoorthy1997@gmail.com>
2021-02-20 19:02:32 +0530

    ganesh_flow

---
+++ domain-page2.php
@@ -0,0 +1,278 @@
+<?php  include "header.php"?>
+<?php include "sidenav.php"?>
+
+<html>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/domain-page.css">
+<!-- <link rel="stylesheet" href="css/domain-page4.css"> -->
+<script>
+</script>
+
+<body>
+    <?php include "nav-pills.php"?>
+    <div class="main-content">
+        <!-- center content starts -->
+        <div class="row px-3 px-md-5 mt-sm-2 main-content-header">
+
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="pt-md-4">
+                            <div class="borde pt-3 ">
+                                <div class="right-label text-center text-white pr-0">
+                                    <p class="side-label"> Recently Added </p>
+                                </div>
+                                <div>
+                                    <h1 class="heading mx-4 pb-2 mt-3"> Central Acts</h1>
+                                    <marquee direction="up" scrollamount="2" onmouseover="this.stop();"
+                                        onmouseout="this.start();">
+                                        <ul class="mx-3 p-0">
+                                            <li class="content"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag">
+                                                    Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                            <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                            <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                            <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                            <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag">
+                                                    Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                            <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                    href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                    Munispality
+                                                    act,1998</a>
+                                            </li>
+                                        </ul>
+                                    </marquee>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 pt-2 pt-md-0">
+                        <div class="pt-md-4">
+                            <div class="borde pt-3">
+                                <div class="right-label text-center text-white pr-0">
+                                    <p class="side-label"> Recently Added</p>
+                                </div>
+
+                                <h1 class="heading mx-4 pb-2 mt-3"> Central Rules</h1>
+                                <marquee direction="up" scrollamount="2" onmouseover="this.stop();"
+                                    onmouseout="this.start();">
+                                    <ul class="mx-3 p-0">
+                                        <li class="content"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                    </ul>
+                                </marquee>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mt-2">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="pt-md-2">
+                            <div class="borde pt-3">
+                                <div class="right-label  text-center text-white pr-0">
+                                    <p class="side-label"> Recently Added</p>
+                                </div>
+
+                                <h1 class="heading mx-4 pb-2 mt-3"> State Acts</h1>
+                                <marquee direction="up" scrollamount="2" onmouseover="this.stop();"
+                                    onmouseout="this.start();">
+                                    <ul class="mx-3 p-0">
+                                        <li class="content"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                    </ul>
+                                </marquee>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 pt-2 pt-md-0">
+                        <div class="pt-md-2">
+                            <div class="borde pt-3">
+                                <div class="right-label text-center text-white pr-0">
+                                    <p class="side-label"> Recently Added</p>
+                                </div>
+                                <h1 class="heading mx-4 pb-2 mt-3"> State Rules</h1>
+                                <marquee direction="up" scrollamount="2" onmouseover="this.stop();"
+                                    onmouseout="this.start();">
+                                    <ul class="mx-3 p-0">
+                                        <li class="content"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                    </ul>
+                                </marquee>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 mt-2">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="pt-md-2">
+                            <div class="borde pt-3">
+                                <div class="right-label  text-center text-white pr-0">
+                                    <p class="side-label"> Recently Added</p>
+                                </div>
+
+                                <h1 class="heading mx-4 pb-2 mt-3"> Schemes</h1>
+                                <marquee direction="up" scrollamount="2" onmouseover="this.stop();"
+                                    onmouseout="this.start();">
+                                    <ul class="mx-3 p-0">
+                                        <li class="content"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag">
+                                                Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                        <li class="content mt-1"> <i class="fa fa-fw fa-angle-right px-3"></i><a
+                                                href="domain-page8.php" class="anchor_tag"> Uttrapradesh
+                                                Munispality
+                                                act,1998</a>
+                                        </li>
+                                    </ul>
+                                </marquee>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
