f6466448b2805656497dd6c1ea268537796e7f98
Rganeshmoorthy <rganeshmoorthy1997@gmail.com>
2021-02-24 12:19:50 +0530

    new_form

---
+++ complaints.php
@@ -0,0 +1,83 @@
+<?php  include "header.php"?>
+<?php include "sidenav.php" ?>
+<html>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/domain-page8.css">
+<link rel="stylesheet" href="css/domain-page4.css">
+<script>
+jQuery(document).ready(function() {
+    jQuery('.scrollbarnew').scrollbar();
+});
+</script>
+
+<body>
+    <?php include "nav-pills.php"?>
+    <div class="main-content">
+        <!-- center content starts -->
+        <div class="row px-3 px-md-5 ">
+            <div class="col-12 heading text-center">
+                Lets Talk Complaints
+            </div>
+            <div class="col-12 py-2">
+                <div class="d-flex justify-content-center">
+                    <div>
+                        <button type="button" class="btn btn-sm text-white button-head mx-2">View Full Act</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 py-2 px-4 px-md-0 d-flex justify-content-center">
+                <div class="border col-12 col-md-7">
+                    <input type="text" placeholder="search Acts" class="border-0 w-100 pr-4 search-input pl-2">
+                    <button class="border-0 bg-transparent search-btn"><i class="fa fa-search"></i></button>
+                </div>
+
+            </div>
+
+            <div class="table_scroll_redirect scrollbarnew ">
+                <div class="col-12 py-2 px-5">
+                    <table class="table table-bordered table_bgcolor">
+                        <thead class="t-head">
+                            <tr>
+                                <th scope="col" class="">Title</th>
+                                <th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-content">
+                            <tr>
+                                <td>Perpetual License and other proposed amendments under Food Safety and Standards Act,
+                                    2006</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td>A comprehensive list of licenses for Hotel and Restaurant business</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
---
+++ forms.php
@@ -0,0 +1,226 @@
+<?php  include "header.php"?>
+<?php include "sidenav.php" ?>
+<html>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/domain-page8.css">
+<link rel="stylesheet" href="css/domain-page4.css">
+<script>
+jQuery(document).ready(function() {
+    jQuery('.scrollbarnew').scrollbar();
+});
+</script>
+
+<body>
+    <?php include "nav-pills.php"?>
+    <div class="main-content">
+        <!-- center content starts -->
+        <div class="row px-3 px-md-5 ">
+            <div class="col-12 heading text-center">
+                FORMS
+            </div>
+            <div class="col-12 py-2">
+                <div class="d-flex justify-content-center">
+                    <div>
+                        <button type="button" class="btn btn-sm text-white button-head mx-2">View Full Act</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 py-2 px-4 px-md-0 d-flex justify-content-center">
+                <div class="border col-12 col-md-7">
+                    <input type="text" placeholder="search Acts" class="border-0 w-100 pr-4 search-input pl-2">
+                    <button class="border-0 bg-transparent search-btn"><i class="fa fa-search"></i></button>
+                </div>
+
+            </div>
+
+            <div class="table_scroll_redirect scrollbarnew ">
+                <div class="col-12 py-2 px-5">
+                    <table class="table table-bordered">
+                        <thead class="t-head">
+                            <tr>
+                                <th scope="col" class="">Statutory Name</th>
+                                <th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-content">
+                            <tr>
+                                <td>Bio-Medical Waste Management & handling Rules, 2016</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td>Food Safety and Standards Rules, 2011</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Tamil Nadu Lifts Rules, 1997</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Meghalaya Air (Prevention And Conrol Of Pollution) Rules, 1988</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Kerala Municipality Building Rules, 1999</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Karnataka Lifts Escalators and Passenger Conveyors Rules, 2014</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Assam Lifts And Escalators Rules, 2010</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Water (Prevention and Control of Pollution) Rules, 1975</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Pre-Conception and Pre-natal Diagnostic Techniques (Prohibition of Sex Selection)
+                                    Rules, 1996</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Plastic Waste Management Rules, 2015</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Batteries (Management and Handling) Rules, 2001</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Dangerous Machines (Regulation) Rules, 2007</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Air (Prevention and Control of Pollution) (Union Territories) Rules, 1983</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Copyrights Rules, 1958</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Coir Industry Rules, 1954</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                    <button type="button"
+                                        class="btn btn-sm text-white button-readmore mx-2 mt-2 mt-md-0"
+                                        onclick="window.location.href='act-definition.php'">View
+                                        Files</button>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
