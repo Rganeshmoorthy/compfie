858e1157be35bd1f4cf693d52a7a6850a3ec568f
Rganeshmoorthy <rganeshmoorthy1997@gmail.com>
2021-03-01 19:13:40 +0530

    kk

--- nav-pills.php
+++ nav-pills.php
@@ -10,14 +10,14 @@
 <body>
     <div class="container-fluid nav_pill w-100 p-0">
         <ul id="myTabs" class="nav nav-pills nav-justified nav_ul w-100 ">
-            <li class="px-5 px-sm-0 px-md-1 px-xl-5  ml-0 ml-md-5  nav-li"><a href="domain-page.php"
+            <li class="px-2 px-sm-0 px-md-1 px-xl-5  ml-0 ml-md-5  nav-li"><a href="domain-page.php"
                     class="px-3">Labour,Employment &
                     Industrial</a>
             </li>
-            <li class="px-5 px-sm-0 px-md-1 px-xl-5 nav-li"><a href="domain-page1.php" class="px-3">Environmental,Health
+            <li class="px-2 px-sm-0 px-md-1 px-xl-5 nav-li"><a href="domain-page1.php" class="px-3">Environmental,Health
                     & Safety</a>
             </li>
-            <li class="px-5 px-sm-0 px-md-1 px-xl-5 nav-li"><a href="domain-page2.php" class="px-3">Fiscal,Corporate &
+            <li class="px-2 px-sm-0 px-md-1 px-xl-5 nav-li"><a href="domain-page2.php" class="px-3">Fiscal,Corporate &
                     Commercial</a>
             </li>
         </ul>
