7ebf026d68b524f32cccd83f799dc6553e0b082d
ak <ak@waioz.com>
2021-04-20 18:43:18 +0530

    new changes for header button

--- nav-pills.php
+++ nav-pills.php
@@ -36,32 +36,33 @@
 
 </head>
 
-<body>  
-  <div>
-    <div class="container-fluid nav_pill p-0 new-nav scrollbarnew" id="container">
-        <ul id="myTabs" class="d-flex nav-pills nav-justified nav_ul pills-ul">
-            <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page.php" class="px-3">Labour,Employment &
-                    Industrial</a>
-            </li>
-            <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page1.php" class="px-3">Environmental,Health
-                    & Safety</a>
-            </li>
-            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page2.php" class="px-3">Fiscal,Corporate &
-                    Commercial</a>
-            </li>
-            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page3.php" class="px-3">Labour,Employment &
-                    Industrial</a>
-            </li>
-            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page4.php" class="px-3">Environmental,Health
-                    & Safety</a>
-            </li>
-            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page5.php" class="px-3">Fiscal,Corporate &
-                    Commercial</a>
-            </li>
-        </ul>
+<body>
+    <div class="position-relative">
+        <div class="container-fluid nav_pill p-0 new-nav scrollbarnew" id="container">
+            <ul id="myTabs" class="d-flex nav-pills nav-justified nav_ul pills-ul">
+                <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page.php" class="px-3">Labour,Employment &
+                        Industrial</a>
+                </li>
+                <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page1.php" class="px-3">Environmental,Health
+                        & Safety</a>
+                </li>
+                <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page2.php" class="px-3">Fiscal,Corporate &
+                        Commercial</a>
+                </li>
+                <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page3.php" class="px-3">Labour,Employment &
+                        Industrial</a>
+                </li>
+                <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page4.php" class="px-3">Environmental,Health
+                        & Safety</a>
+                </li>
+                <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page5.php" class="px-3">Fiscal,Corporate &
+                        Commercial</a>
+                </li>
+            </ul>
+        </div>        
+        <button id="slideBack" type="button"><i class="fa fa-arrow-left color" aria-hidden="true"></i></button>         
+        <button id="slide" type="button"><i class="fa fa-arrow-right color" aria-hidden="true"></i></button>        
     </div>
-    <button id="slideBack" type="button"><i class="fa fa-arrow-left color" aria-hidden="true"></i></button>
-    <button id="slide" type="button"><i class="fa fa-arrow-right color" aria-hidden="true"></i></button>  </div>
 </body>
 
 </html>
