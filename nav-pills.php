8fe86ba8058943818871ea4ff999d02ce6f51a5f
ak <ak@waioz.com>
2021-04-20 17:08:10 +0530

    sdfds

--- nav-pills.php
+++ nav-pills.php
@@ -3,45 +3,44 @@
 <head>
     <link rel="stylesheet" href="css/nav-pill.css">
     <script>
-        jQuery(document).ready(function() {
+    jQuery(document).ready(function() {
         jQuery('.scrollbarnew').scrollbar();
-            
-  $('#slide').hover(function() {
-    console.log('gakbjdl')
-    var container = document.getElementById('container');
-    sideScroll(container,'right',25,900,10);
-});
 
-$('#slideBack').hover(function() {
-    var container = document.getElementById('container');
-    sideScroll(container,'left',25,900,10);
-});
+        $('#slide').hover(function() {
+            console.log('gakbjdl')
+            var container = document.getElementById('container');
+            sideScroll(container, 'right', 25, 900, 10);
+        });
 
-function sideScroll(element,direction,speed,distance,step){
-    scrollAmount = 0;
-    var slideTimer = setInterval(function(){
-        if(direction == 'left'){
-            element.scrollLeft -= step;
-        } else {
-            element.scrollLeft += step;
+        $('#slideBack').hover(function() {
+            var container = document.getElementById('container');
+            sideScroll(container, 'left', 25, 900, 10);
+        });
+
+        function sideScroll(element, direction, speed, distance, step) {
+            scrollAmount = 0;
+            var slideTimer = setInterval(function() {
+                if (direction == 'left') {
+                    element.scrollLeft -= step;
+                } else {
+                    element.scrollLeft += step;
+                }
+                scrollAmount += step;
+                if (scrollAmount >= distance) {
+                    window.clearInterval(slideTimer);
+                }
+            }, speed);
         }
-        scrollAmount += step;
-        if(scrollAmount >= distance){
-            window.clearInterval(slideTimer);
-        }
-    }, speed);
-}
-}); 
-
+    });
     </script>
 
 </head>
 
-<body>
-    <div class="container-fluid nav_pill p-0 new-nav scrollbarnew" id="container">      
+<body>  
+  <div>
+    <div class="container-fluid nav_pill p-0 new-nav scrollbarnew" id="container">
         <ul id="myTabs" class="d-flex nav-pills nav-justified nav_ul pills-ul">
-            <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page.php"
-                    class="px-3">Labour,Employment &
+            <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page.php" class="px-3">Labour,Employment &
                     Industrial</a>
             </li>
             <li class="px-2 px-sm-0 px-md-1 nav-li"><a href="domain-page1.php" class="px-3">Environmental,Health
@@ -49,21 +48,20 @@ function sideScroll(element,direction,speed,distance,step){
             </li>
             <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page2.php" class="px-3">Fiscal,Corporate &
                     Commercial</a>
-            </li>      
-              <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page3.php" class="px-3">Labour,Employment &
+            </li>
+            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page3.php" class="px-3">Labour,Employment &
                     Industrial</a>
-            </li> 
-              <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page4.php" class="px-3">Environmental,Health
+            </li>
+            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page4.php" class="px-3">Environmental,Health
                     & Safety</a>
-            </li> 
-              <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page5.php" class="px-3">Fiscal,Corporate &
+            </li>
+            <li class="px-2 px-sm-0 px-md-2 nav-li"><a href="domain-page5.php" class="px-3">Fiscal,Corporate &
                     Commercial</a>
-            </li>                  
-    </ul>           
-    </div>   
+            </li>
+        </ul>
+    </div>
     <button id="slideBack" type="button"><i class="fa fa-arrow-left color" aria-hidden="true"></i></button>
-    <button id="slide" type="button"><i class="fa fa-arrow-right color" aria-hidden="true"></i></button> 
-
+    <button id="slide" type="button"><i class="fa fa-arrow-right color" aria-hidden="true"></i></button>  </div>
 </body>
 
 </html>
