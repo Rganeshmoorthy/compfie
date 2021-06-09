a58775e8221807a0ebb226df84f7e4788aec6f57
ganesh <ganesh@waioz.com>
2021-06-09 12:01:40 +0530

    uy

---
+++ recently-state-act.php
@@ -0,0 +1,43 @@
+<?php  include "header.php"?>
+<?php include "sidenav.php" ?>
+<html>
+<link rel="stylesheet" href="css/index.css">
+<script>
+
+</script>
+
+<body>
+    <div class="main-content">
+        <!-- center content starts -->
+
+        <div class="row px-md-5 py-5">        
+         <a href="domain-page4.php"><button type="button" class="bg-bt text-white pr-2"><i
+                        class="fa fa-arrow-left text-white px-1 nav-prev" aria-hidden="true"></i>Goto State Acts</button></a>    
+            <div class="heading col-12 pb-4 d-flex justify-content-center">
+                Recently Added
+            </div>   
+            <div class="col-12 recent-added scrollbarnew">
+                <ul class="result-ul">
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Air (Prevention and Control of Pollution) Act, 1981 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Clinical Establishments (Registration And Regulation) Act, 2010 [02.06.2012]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php">  *Environment (Protection) Act, 1986 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php">  *Assam Lifts And Escalators Act, 1990 [008.03.2010]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Fatal Accidents Act, 1900  [19.07.2015]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> Cine - Workers and Cinema Theatre Workers (Regulation of Employment) Act, 1981 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Fatal Accidents Act, 1900  [20.12.2016]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Coal Mines Provident Fund and Miscellaneous Provisions Act, 1948 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Contract Labour (Regulation and Abolition) Act, 1970  [22.09.2019]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Cine - Workers and Cinema Theatre Workers (Regulation of Employment) Act, 1981 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Children (Pledging of Labour) Act, 1933 [11.11.2020]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Building and Other Construction Workers (Regulation of Employment and Conditions of Service) Act, 1996 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Building and Other Construction Workers' Welfare Cess Act, 1996 [01.02.2008]</a></li>
+                    <li class="py-2 recent"><a href="redirect-state-act.php"> *Beedi and Cigar Workers (Conditions of Employment) Act, 1966 [30.10.2021]</a></li>
+                </ul>
+            </div>            
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
---
+++ redirect-scheme.php
@@ -0,0 +1,166 @@
+<?php  include "header.php"?>
+<?php include "sidenav.php" ?>
+<html>
+<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/domain-page8.css">
+<link rel="stylesheet" href="css/domain-page4.css">
+<script>
+jQuery(document).ready(function() {
+    jQuery('.scrollbarnew').scrollbar();
+});
+$(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
+</script>
+
+<body>
+
+    <div class="main-content">
+        <!-- center content starts -->
+        <div class="row px-3 px-md-5 ">
+            <a href="schemes.php"><button type="button" class="bg-bt text-white pr-2"><i
+                        class="fa fa-arrow-left text-white px-1 nav-prev" aria-hidden="true"></i>Goto Schemes</button></a>
+            <div class="col-12 heading text-center">
+            Schemes
+            </div>
+            <div class="col-12 py-2">
+                <div class="d-flex justify-content-center">
+                    <div>
+                        <button type="button" class="btn btn-sm text-white button-head mx-2"
+                            onclick="window.location.href='full-act.php'">View Full Act</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 py-2 px-4 px-md-0 d-flex justify-content-center">
+                <div class="col-12 col-md-7 table_search">
+                    <input type="text" id="myInput" placeholder="Search Acts"
+                        class="border-0 w-100 pr-4 search-input pl-2">
+                    <button class="border-0 bg-transparent search-btn"><i class="fa fa-search"></i></button>
+                </div>
+
+            </div>
+
+            <div class=" col-12">
+                <div class="col-12 py-2 px-md-5 table-wrap scrollbarnew">
+                    <table class="table table-striped table-borderless">
+                        <thead class="t-head">
+                            <tr>
+                                <th scope="col" class="">Section Number</th>
+                                <th scope="col" class="">Section Title</th>
+                                <th scope="col" class="">Section</th>
+                                <th scope="col" class="">Discription</th>
+                                <th scope="col" class=""></th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-content" id="myTable">
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Environmental,Health & Safety</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Defnition</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm  button-readmore-white mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Qualification</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Contract of Apprenticeship</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm button-readmore-white mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+
+                            <tr class="text-center border">
+                                <td colspan="5">
+                                    CHAPTER II - CENTRAL AND STATE BOARDS FOR THE PREVENTION AND CONTROL
+                                    OF AIR
+                                    POLLUTION</td>
+                            </tr>
+
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Environmental,Health & Safety</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm button-readmore-white mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Environmental,Health & Safety</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td>
+                                    <button type="button" class="btn btn-sm button-readmore-white mx-2 mt-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Environmental,Health & Safety</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm text-white button-readmore mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">4</th>
+                                <td>Environmental,Health & Safety</td>
+                                <td>Lorem ipsum dolor sit amet, consectetur</td>
+                                <td>Lorem ipsum dolor sit amet,</td>
+                                <td> <button type="button" class="btn btn-sm button-readmore-white mx-2"
+                                        onclick="window.location.href='act-definition.php'">Read
+                                        more</button>
+                                </td>
+                            </tr>
+
+
+
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+        <!-- center content ends -->
+        <?php  include "footer.php"?>
+    </div>
+</body>
+
+</html>
